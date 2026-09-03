477bd945a907b94cacdf54c7a7a612dadd60bac0
Abdoul Hamid Coulibaly <abdoulhamid.coulibaly.ext@orange.com>
2026-09-03 12:30:47 +0200

    chore: update dependencies, licenses, and configs

--- pkg/errors/new.go
+++ pkg/errors/new.go
@@ -15,7 +15,7 @@ import (
 )
 
 // Newf creates a new error with a formatted message.
-func Newf(format string, args ...interface{}) error {
+func Newf(format string, args ...any) error {
 	return fmt.Errorf(format, args...)
 }
 
