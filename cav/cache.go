fdef97b843c396cc59c9816c8c1c2e100d96970a
Abdoul Hamid Coulibaly <abdoulhamid.coulibaly.ext@orange.com>
2026-09-10 15:15:24 +0200

    refactor: remove legacy command runtime from typed stack

--- cav/cache.go
+++ cav/cache.go
@@ -10,14 +10,7 @@
 package cav
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
-	"encoding/base64"
-	"encoding/gob"
 	"encoding/json"
-	"errors"
-	"io"
 	"os"
 )
 
@@ -85,68 +78,4 @@ func (c *client) restoreSessionsFromCache(passphrase, path string) error {
 	}
 
 	return nil
-}
-
-func encryptSessions(key []byte, plaintext string) (string, error) {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return "", err
-	}
-
-	plaintextBytes := []byte(plaintext)
-	ciphertext := make([]byte, aes.BlockSize+len(plaintextBytes))
-
-	iv := ciphertext[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		return "", err
-	}
-
-	stream := cipher.NewCTR(block, iv)
-	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintextBytes)
-
-	return base64.URLEncoding.EncodeToString(ciphertext), nil
-}
-
-func decryptSessions(key []byte, ciphertext string) (string, error) {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return "", err
-	}
-
-	ciphertextBytes, err := base64.URLEncoding.DecodeString(ciphertext)
-	if err != nil {
-		return "", err
-	}
-
-	if len(ciphertextBytes) < aes.BlockSize {
-		return "", errors.New("ciphertext is too short")
-	}
-
-	iv := ciphertextBytes[:aes.BlockSize]
-	ciphertextBytes = ciphertextBytes[aes.BlockSize:]
-
-	stream := cipher.NewCTR(block, iv)
-	stream.XORKeyStream(ciphertextBytes, ciphertextBytes)
-
-	return string(ciphertextBytes), nil
-}
-
-func writeGobFile(path string, data interface{}) error {
-	file, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	encoder := gob.NewEncoder(file)
-	return encoder.Encode(data)
-}
-
-func readGobFile(path string, data interface{}) error {
-	file, err := os.Open(path)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	decoder := gob.NewDecoder(file)
-	return decoder.Decode(data)
 }
