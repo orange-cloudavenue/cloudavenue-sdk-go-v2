8eab3984310c6b767d381650dc37ec0820ee96a2
Abdoul Hamid Coulibaly <abdoulhamid.coulibaly.ext@orange.com>
2026-09-10 13:01:25 +0200

    fix(cav): harden job polling and cache handling

--- cav/await_job_test.go
+++ cav/await_job_test.go
@@ -75,6 +75,11 @@ func TestAwaitJobOnBackendUnsupportedBackend(t *testing.T) {
 	require.EqualError(t, err, "await job job-id: backend 4 does not support jobs")
 }
 
+func TestGetJobEndpointNameUnsupportedBackend(t *testing.T) {
+	_, err := getJobEndpointName(BackendNetBackup)
+	require.EqualError(t, err, "backend 4 does not support jobs")
+}
+
 func TestClassifyStatusCode(t *testing.T) {
 	require.ErrorIs(t, classifyStatusCode(404), pkgerrors.ErrNotFound)
 	require.NoError(t, classifyStatusCode(500))
